766a999c7181b71e58bb546e97a000dbc6c09518
AS <38606123+anttiope@users.noreply.github.com>
2024-09-25 18:31:33 +0000

    Prettier formatting

--- js/script.js
+++ js/script.js
@@ -1,53 +1,57 @@
 function makeSmall() {
-    const textBox = document.getElementById('textBox');
-    textBox.value = textBox.value.toLowerCase();
+  const textBox = document.getElementById("textBox");
+  textBox.value = textBox.value.toLowerCase();
 }
 
 function makeBig() {
-    const textBox = document.getElementById('textBox');
-    textBox.value = textBox.value.toUpperCase();
+  const textBox = document.getElementById("textBox");
+  textBox.value = textBox.value.toUpperCase();
 }
 
 async function copyToClipboard() {
-    const textBox = document.getElementById('textBox');
-    if (navigator.clipboard.writeText) { // if the new method to copy to clipboard is available...
-	try {
-	    await navigator.clipboard.writeText(textBox.value); // use the new method for copying to clipboard
-	    textBox.value = ''; // clear text box to give feedback to user
-        } catch (err) {
-	    console.error('Failed to copy: ',err);
-	}
-    } else {
-	try {
-	    textBox.select();
-	    await document.execCommand('copy'); // old method for copying to clipboard
-	    textBox.value = '';
-	} catch (err) {
-	    console.error('Fallback copy failed: ', err);
-	}
+  const textBox = document.getElementById("textBox");
+  if (navigator.clipboard.writeText) {
+    // if the new method to copy to clipboard is available...
+    try {
+      await navigator.clipboard.writeText(textBox.value); // use the new method for copying to clipboard
+      textBox.value = ""; // clear text box to give feedback to user
+    } catch (err) {
+      console.error("Failed to copy: ", err);
     }
+  } else {
+    try {
+      textBox.select();
+      await document.execCommand("copy"); // old method for copying to clipboard
+      textBox.value = "";
+    } catch (err) {
+      console.error("Fallback copy failed: ", err);
+    }
+  }
 }
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    const themeToggleBtn = document.getElementById('theme-toggle');
-    const currentTheme = localStorage.getItem('theme');
+document.addEventListener("DOMContentLoaded", (event) => {
+  const themeToggleBtn = document.getElementById("theme-toggle");
+  const currentTheme = localStorage.getItem("theme");
 
-    if (currentTheme) {
-        document.body.classList.add(currentTheme);
-        if (currentTheme === 'dark-mode') {
-            themeToggleBtn.innerHTML = '<img src="/icons/sun-solid.svg" alt="Light mode button" class="icon"> Light mode';
-        }
+  if (currentTheme) {
+    document.body.classList.add(currentTheme);
+    if (currentTheme === "dark-mode") {
+      themeToggleBtn.innerHTML =
+        '<img src="/icons/sun-solid.svg" alt="Light mode button" class="icon"> Light mode';
     }
+  }
 
-    themeToggleBtn.addEventListener('click', function() {
-        document.body.classList.toggle('dark-mode');
-        let theme = 'light-mode';
-        if (document.body.classList.contains('dark-mode')) {
-            theme = 'dark-mode';
-            themeToggleBtn.innerHTML = '<img src="/icons/sun-solid.svg" alt="Light mode button" class="icon"> Light mode';
-        } else {
-            themeToggleBtn.innerHTML = '<img src="/icons/moon-solid.svg" alt="Dark mode button" class="icon"> Dark mode';
-        }
-        localStorage.setItem('theme', theme);
-    });
+  themeToggleBtn.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+    let theme = "light-mode";
+    if (document.body.classList.contains("dark-mode")) {
+      theme = "dark-mode";
+      themeToggleBtn.innerHTML =
+        '<img src="/icons/sun-solid.svg" alt="Light mode button" class="icon"> Light mode';
+    } else {
+      themeToggleBtn.innerHTML =
+        '<img src="/icons/moon-solid.svg" alt="Dark mode button" class="icon"> Dark mode';
+    }
+    localStorage.setItem("theme", theme);
+  });
 });
